afa87a65d4900d1778595fc1dd3aa449a0c33a03
Breakthrough <temitopefatoki658@gmail.com>
2023-05-30 16:57:24 -0700

    Resolving unknown conversion specifier

---
+++ 2-format_printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * _printf - function that prints accroding to format
+  * @format: argument received
+  * @...: ellipsis
+  * Return: incrememnt the character and integer
+  */
+int _printf(const char *format, ...)
+{
+	int ch, d;
+	char *str;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				ch = va_arg(args, int);
+				printf("%d", ch);
+			}
+			if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				printf("%s", str);
+			}
+			else if (*format == 'd' || 'i')
+			{
+				d = va_arg(args, int);
+				printf("%d", d);
+			}
+			else if (*format == '%')
+			{
+				printf("%%");
+			}
+			else if(*format == 'r')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				format++;
+			}
+			else
+			{
+				printf("Invalid specifier: %c", *format);
+			}
+		}
+		
+		else
+			putchar(*format);
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (0);
+}

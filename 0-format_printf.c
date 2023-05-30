6908a7228354bb5d06bde82a134b6cf42b75c894
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-05-30 08:06:36 -0700

    0-format_printf testing

--- 0-format_printf.c
+++ 0-format_printf.c
@@ -1,81 +1,60 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
-  * _print - printf function entry point to print out according to format
-  *
-  * @format: an argument that printf function will recieve
-  *
-  * @...: elipsis
-  *
-  * Return: increment of the characters and integers
-  */
-
-
+* _print - printf function entry point to print out according to format
+*
+* @format: an argument that printf function will recieve
+*
+* @...: elipsis
+*
+* Returnii: increment the characters and integers
+*/
 int _printf(const char *format, ...)
+
+int increment = 0, c;
+va_list args;
+char *str;
+
+va_start(args, format);
+
+while (*format)
 {
-	int increment = 0;
-	va_list args;
-	
-	va_start(args, format);
-
-
-	while (*format != '\0')
+	if (*format == '%')
 	{
-		if (*format == '%')
-		{
-			format++;
+		format++;
 
-			switch (*format)
-			{
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-						while (*str != '\0')
-						{
-							putchar(*str);
-							str++;
-							increment++;
-						}
-					}
-					break;
-
-				case 'c':
-					{
-						int c = (char)va_arg(args, int);
-						putchar(c);
-						increment++;
-						break;
-					}
-
-				case '%':
-					{
-						putchar('%');
-						increment++;
-						break;
-					}
-
-				default:
-					{
-						putchar('%');
-						putchar(*format);
-						increment += 2;
-						break;
-					}
-			}
-		}
-			else
-			{
-				putchar(*format);
-				increment++;
-			}
-
-			format++;
-		}
-
-		va_end(args);
-
-
-		return (increment);
+	if (*format == 'c')
+	{
+		c = va_arg(args, int);
+		putchar(c);
+		increment++;
 	}
+	else if (*format == 's')
+	{
+		*str = va_arg(args, char *);
+
+		while (*str)
+		{
+			putchar(*str);
+			increment++;
+			str++;
+		}
+	}
+	else if (*format == '%')
+	{
+		putchar('%');
+		increment++;
+	}
+	}
+	else
+	{
+		(*format);
+		increment++;
+	}
+	format++;
+}
+va _end(args);
+return increment;
+}
+
